6d6c0759a590784ba1a77444d5a71b3a223a39f5
Dawid Nowak <contact@dnowak.pl>
2013-12-27 20:23:43 +0100

    index.php: wyliczenia na podstawie danych z bazki wyciągnięte do funkcji get_avarage_payout(), get_daily_payout() i get_total_payout()

--- src/index.php
+++ src/index.php
@@ -15,40 +15,56 @@
     /**
      * Liczymy $payout_average
      */
-    $check = 'SELECT * FROM logs' or die('Error in the consult…'.mysqli_error($link));
-    $result = mysqli_query($link, $check);
-    $payout_average = 0;
+    function get_avarage_payout($link)
+    {
+        $check = 'SELECT * FROM logs' or die('Error in the consult…'.mysqli_error($link));
+        $result = mysqli_query($link, $check);
+        $payout_average = 0;
 
-    while ($row = mysqli_fetch_array($result)) {
-        $payout_average = $payout_average + $row['payout'];
+        while ($row = mysqli_fetch_array($result)) {
+            $payout_average = $payout_average + $row['payout'];
+        }
+
+        $am = mysqli_num_rows($result);
+
+        return $payout_average / $am;
     }
-
-    $am = mysqli_num_rows($result);
-    $payout_average = $payout_average / $am;
-
 
     /**
      * Liczymy $payout_daily
      */
-    $check = 'SELECT * FROM logs WHERE DATE(date) = DATE(NOW())' or die('Error in the consult…'.mysqli_error($link));
-    $result2 = mysqli_query($link, $check);
-    $payout_daily = 0;
+    function get_daily_payout($link)
+    {
+        $check = 'SELECT * FROM logs WHERE DATE(date) = DATE(NOW())' or die('Error in the consult…'.mysqli_error($link));
+        $result2 = mysqli_query($link, $check);
+        $payout_daily = 0;
 
-    while ($row = mysqli_fetch_array($result2)) {
-        $payout_daily = $payout_daily + $row['payout'];
+        while ($row = mysqli_fetch_array($result2)) {
+            $payout_daily = $payout_daily + $row['payout'];
+        }
+
+        return $payout_daily;
     }
-
 
     /**
      * Liczymy $payout_total
      */
-    $check = 'SELECT * FROM logs' or die('Error in the consult…'.mysqli_error($link));
-    $result2 = mysqli_query($link, $check);
-    $payout_total = 0;
+    function get_total_payout($link)
+    {
+        $check = 'SELECT * FROM logs' or die('Error in the consult…'.mysqli_error($link));
+        $result2 = mysqli_query($link, $check);
+        $payout_total = 0;
 
-    while ($row = mysqli_fetch_array($result2)) {
-        $payout_total = $payout_total + $row['payout'];
+        while ($row = mysqli_fetch_array($result2)) {
+            $payout_total = $payout_total + $row['payout'];
+        }
+
+        return $payout_total;
     }
+
+    $payout_average = get_avarage_payout($link);
+    $payout_daily = get_daily_payout($link);
+    $payout_total = get_total_payout($link);
 
 ?><!DOCTYPE html>
 <html lang="en">
