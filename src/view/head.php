9e63a5c10eeb1d3f11fcb83874154a1113f78bad
Dawid Nowak <contact@dnowak.pl>
2013-12-29 02:44:14 +0100

    Widok "template.php" rozdzielony na podwidoki "index.php", "head.php", "header.php", "footer.php" dla łatwiejszego zarządzania

---
+++ src/view/head.php
@@ -0,0 +1,30 @@
+<?php defined('APP_PATH') OR die('Access denied'); ?>
+
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="">
+<meta name="author" content="">
+<link rel="shortcut icon" href="../../docs-assets/ico/favicon.png">
+
+<title>FAUCET | dogecoins.pl</title>
+
+<!-- Bootstrap core CSS -->
+<link href="css/bootstrap.css" rel="stylesheet">
+
+<!-- Custom styles for this template -->
+<link href="sticky-footer-navbar.css" rel="stylesheet">
+
+<!-- Just for debugging purposes. Don't actually copy this line! -->
+<!--[if lt IE 9]><script src="../../docs-assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+<![endif]-->
+<link
+    href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic"
+    rel="stylesheet"
+    type="text/css"
+/>
---
+++ src/view/header.php
@@ -0,0 +1,26 @@
+<?php defined('APP_PATH') OR die('Access denied'); ?>
+
+<!-- Fixed navbar -->
+<div class="navbar navbar-default navbar-fixed-top" role="navigation">
+    <div class="container">
+        <div class="navbar-header">
+            <button
+                type="button"
+                class="navbar-toggle"
+                data-toggle="collapse"
+                data-target=".navbar-collapse"
+            >
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Faucet</a>
+        </div>
+        <div class="collapse navbar-collapse right">
+            <ul class="nav navbar-nav">
+                <li><a href="http://dogecoins.pl/">Back to dogecoins.pl</a></li>
+            </ul>
+        </div><!--/.nav-collapse -->
+    </div>
+</div>
